1c1ded4241e46a71c91925531d0dea7406621fd5
elias <e.detlefsen@student.fontys.nl>
2025-10-10 15:01:50 +0200

    CI test

--- src/test/java/com/fontys/marketplace_backend/MarketplaceBackendApplicationTests.java
+++ src/test/java/com/fontys/marketplace_backend/MarketplaceBackendApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -30,11 +31,15 @@ class MarketplaceBackendApplicationTests {
         mockMvc.perform(post("/signup")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"displayName\":\"test123\", \"email\":\"test@example.com\", \"password\":\"password123\"}"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print());
 
         mockMvc.perform(post("/login")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"email\":\"test@example.com\", \"password\":\"password123\"}"))
-                .andExpect(status().isOk());
+                        .content("{\"email\":\"test@example.com\", \"password\":\"password123\"}")
+                        .sessionAttr("someSessionAttribute", "value")) // If session is required
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+
     }
 }
