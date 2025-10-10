70cb393a722daff09ab570ac2577b96d5507b8bb
elias <e.detlefsen@student.fontys.nl>
2025-10-10 14:29:43 +0200

    CI test

--- src/test/java/com/fontys/marketplace_backend/MarketplaceBackendApplicationTests.java
+++ src/test/java/com/fontys/marketplace_backend/MarketplaceBackendApplicationTests.java
@@ -1,11 +1,13 @@
 package com.fontys.marketplace_backend;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -13,8 +15,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(classes = MarketplaceBackendApplication.class)
 @AutoConfigureMockMvc
 class MarketplaceBackendApplicationTests {
-    @Autowired
     private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup(WebApplicationContext context) {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
 
     @Test
     public void alwaysOk() throws Exception {
