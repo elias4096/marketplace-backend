8b7fa00061f546690e27bae76aa5bc88c1a7f65d
elias <e.detlefsen@student.fontys.nl>
2025-10-10 14:32:55 +0200

    CI test

--- src/test/java/com/fontys/marketplace_backend/MarketplaceBackendApplicationTests.java
+++ src/test/java/com/fontys/marketplace_backend/MarketplaceBackendApplicationTests.java
@@ -2,7 +2,6 @@ package com.fontys.marketplace_backend;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -13,7 +12,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = MarketplaceBackendApplication.class)
-@AutoConfigureMockMvc
 class MarketplaceBackendApplicationTests {
     private MockMvc mockMvc;
 
