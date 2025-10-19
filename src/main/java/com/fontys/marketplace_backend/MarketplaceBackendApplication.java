f157e9ce74580dfdb49bc1ce575aefb0d895ee5a
elias <e.detlefsen@student.fontys.nl>
2025-10-19 17:51:01 +0200

    Last test?

--- src/main/java/com/fontys/marketplace_backend/MarketplaceBackendApplication.java
+++ src/main/java/com/fontys/marketplace_backend/MarketplaceBackendApplication.java
@@ -6,34 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // Note to self: -p [host port]:[container port] (maps container port to host port)
 
 /*
---- Solution - Dockerfile ---
-
-FROM amazoncorretto:17
-ADD target/back-end.jar /opt/app.jar
-CMD ["java", "-jar", "/opt/app.jar"]
-
---- Solution - build and run ---
-
-# maven build the project
-$ cd back-end
-$ mvn verify
-
-# Build the image and 'tag' it with the name 'back-end'
-$ docker build -t back-end .
-
-# Run it by image-name and give the container also the name 'back-end'
-# port bind host port 10000 to container port 8080 where payara runs.
-$ docker run -d --rm --name back-end -p 10000:8080 back-end
-
---- Solution - Stopping the container ---
-
-# Clean stop
-docker stop back-end
-#or just stop and remove
-docker rm -f back-end
- */
-
-/* Useful commands:
  * Build project:
  * ./gradlew assemble
  *
@@ -49,7 +21,7 @@ docker rm -f back-end
  * Run container:
  * docker run -p 9090:8080 marketplace-backend
  *
- * Stop container
+ * Stop container:
  * docker stop marketplace-backend
  *
  * MySQL docker:
@@ -58,8 +30,8 @@ docker rm -f back-end
  * SonarQube docker:
  * docker run --name sonarqube -p 9000:9000 sonarqube:community
  *
- * Run sonarqube
- * gradlew sonar -Dsonar.token=???
+ * Run sonarqube:
+ * gradlew sonar -Dsonar.token=[token]
  */
 
 @SpringBootApplication
