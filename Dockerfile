45cae4d240fc235033502d85f76066db1250adcb
elias <e.detlefsen@student.fontys.nl>
2025-11-22 18:07:48 +0100

    Small adjustments

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
+# Todo: add gradlew build
+
 FROM eclipse-temurin:21
 
 COPY build/libs/marketplace-backend-0.0.1-SNAPSHOT.jar marketplace-backend.jar
--- src/main/java/com/fontys/marketplace_backend/presentation/controller/ItemController.java
+++ src/main/java/com/fontys/marketplace_backend/presentation/controller/ItemController.java
@@ -11,38 +11,39 @@ import com.fontys.marketplace_backend.persistence.repository.ItemRepository;
 import com.fontys.marketplace_backend.persistence.requests.DeleteItemRequest;
 
 @RestController
+@RequestMapping("/items")
 @CrossOrigin(origins = "http://localhost:5173/")
 @RequiredArgsConstructor
 public class ItemController {
     private final ItemRepository itemRepository;
 
-    @GetMapping("/items/{itemId}")
-    private Optional<Item> getItemById(@PathVariable Integer itemId) {
+    @GetMapping("/{itemId}")
+    Optional<Item> getItemById(@PathVariable Integer itemId) {
         return itemRepository.findById(itemId);
     }
 
     @GetMapping("/items")
-    private Iterable<Item> getItems() {
+    Iterable<Item> getItems() {
         return itemRepository.findAll();
     }
 
     @GetMapping("/user/items/{userId}")
-    private Iterable<Item> getItemsByUserId(@PathVariable Integer userId) {
+    Iterable<Item> getItemsByUserId(@PathVariable Integer userId) {
         return itemRepository.findAllBySellerUserId(userId);
     }
 
     @PostMapping("/items")
-    private Item createItem(@RequestBody Item item) {
+    Item createItem(@RequestBody Item item) {
         return itemRepository.save(item);
     }
 
     @PutMapping("/user/items")
-    private Item updateItem(@RequestBody Item item) {
+    Item updateItem(@RequestBody Item item) {
         return itemRepository.save(item);
     }
 
-    @DeleteMapping("/user/items")
-    private void deleteItem(@RequestBody DeleteItemRequest request) {
-        itemRepository.deleteById(request.getItemId());
+    @DeleteMapping("/user/items/{itemId}")
+    void deleteItem(@PathVariable Integer itemId) {
+        itemRepository.deleteById(itemId);
     }
 }
